030299ff95c94799d8ae5f56f40f15ce76d22291
MuhammadShahrukh731 <muhammadshahrukh731@rocketmail.com>
2019-10-11 15:42:22 +0500

    remove rubucop error

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Pro
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
- config.middleware.use Rack::MethodOverride
+    config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Flash
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
